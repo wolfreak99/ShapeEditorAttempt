10c76681ae95e6d5cf3f505d92178d6fd946da96
wolfreak99 <jbminor1991@gmail.com>
2018-09-20 09:17:37 -0400

    Use AutoType for ShapeButton.Type

--- ShapeEditorAttempt/Controls/ShapeButton.cs
+++ ShapeEditorAttempt/Controls/ShapeButton.cs
@@ -4,18 +4,7 @@ namespace ShapeEditorAttempt
 {
 	public class ShapeButton : ToggleButton
 	{
-		private ShapeType m_type;
-		public ShapeType Type
-		{
-			get
-			{
-				return m_type;
-			}
-			set
-			{
-				m_type = value;
-			}
-		}
+		public ShapeType Type { get; set; }
 
 		public ShapeButton(ShapeType type = ShapeType.Null) : base()
 		{
